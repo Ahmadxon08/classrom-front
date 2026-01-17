bb43bccd6d47ad3634b6308bbc3a5ab856e0efad
Ahmadxon08 <akhmadxonmoydinov@gmail.com>
2026-01-17 17:44:47 +0500

    updated

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -1,5 +1,62 @@
+import { CreateButton } from "@/components/refine-ui/buttons/create";
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
+import { ListView } from "@/components/refine-ui/views/list-view";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { DEPARTMENT_OPTIONS } from "@/constants";
+import { Search } from "lucide-react";
+import { useState } from "react";
+
 const SubjectList = () => {
-  return <div>SubjectList</div>;
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filter, setFilter] = useState("all");
+  return (
+    <ListView>
+      <Breadcrumb />
+      <h1 className="page-title">Subjects</h1>
+
+      <div className="intro-row">
+        <p>Quick access to essential metrics and menagement tools.</p>
+
+        <div className="actions-row">
+          <div className="search-field">
+            <Search className="search-icon" />
+            <Input
+              type="text"
+              placeholder="Search by name..."
+              className="w-full pl-10"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+
+          <div className="flex gap-2 w-full sm:w-auto">
+            <Select value={filter} onValueChange={setFilter}>
+              <SelectTrigger>
+                <SelectValue placeholder="Filter by department" />
+
+                <SelectContent>
+                  <SelectItem value="all">All Departments</SelectItem>
+                  {DEPARTMENT_OPTIONS.map((dept) => (
+                    <SelectItem key={dept.value} value={dept.value}>
+                      {dept.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </SelectTrigger>
+            </Select>
+            <CreateButton />
+          </div>
+        </div>
+      </div>
+    </ListView>
+  );
 };
 
 export default SubjectList;
